7015f70f0f95ebcaaa572867d36894b8f9b1cde0
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-05 23:54:48 +0000

    fixed bot_interface loop bug

--- lib/bot_interface.py
+++ lib/bot_interface.py
@@ -6,6 +6,8 @@ class BotInterface:
         self.row_width = 10
         self.col_width = 10
         self.valid_new_ship_placement = False
+        self.ship_row = 0
+        self.ship_col = 0
 
     def run(self, runs = 5):
         while len(self.game.ships_unplaced) > 0:
@@ -16,7 +18,7 @@ class BotInterface:
                 self.valid_new_ship_placement = False
             break
 
-        self._format_board()
+        print(self._format_board())
 
     def ships_unplaced(self):
         ship_lengths = [str(ship.length) for ship in self.game.ships_unplaced]
@@ -27,17 +29,17 @@ class BotInterface:
             if 0 < int(self.ship_col) < 11:
                 if self.ship_orientation == 'v':
                     if 0 < int(self.ship_row) + int(self.ship_length) < 11:
-                        print('v - int(self.ship_row) + int(self.ship_length): ', int(self.ship_row) + int(self.ship_length))
-                        print('self.ship_row: ', self.ship_row)
-                        print('self.ship_col: ', self.ship_col)
+                        # print('v - int(self.ship_row) + int(self.ship_length): ', int(self.ship_row) + int(self.ship_length))
+                        # print('self.ship_row: ', self.ship_row)
+                        # print('self.ship_col: ', self.ship_col)
                         return True
                     else:
                         return False
                 if self.ship_orientation == 'h':
                     if 0 < int(self.ship_col) + int(self.ship_length) < 11:
-                        print('h - int(self.ship_col) + int(self.ship_length): ', int(self.ship_col) + int(self.ship_length))
-                        print('self.ship_row: ', self.ship_row)
-                        print('self.ship_col: ', self.ship_col)
+                        # print('h - int(self.ship_col) + int(self.ship_length): ', int(self.ship_col) + int(self.ship_length))
+                        # print('self.ship_row: ', self.ship_row)
+                        # print('self.ship_col: ', self.ship_col)
                         return True
                     else:
                         return False
@@ -45,15 +47,21 @@ class BotInterface:
             return False
     
     def random_col_row_placement(self):
-        while not self.valid_new_ship_placement:
+        max_attempts = 20  # Adjust as needed
+        attempts = 0
+
+        while not self.valid_new_ship_placement and attempts < max_attempts:
             self.ship_row = random.randint(1, 11 - int(self.ship_length))
-            print('self.ship_row: ', self.ship_row)
+            # print('self.ship_row: ', self.ship_row)
             self.ship_col = random.randint(1, 11 - int(self.ship_length))
-            print('self.ship_col: ', self.ship_col)
+            # print('self.ship_col: ', self.ship_col)
             if self.is_new_ship_placement_inside_board():
-                print('if - here')
                 self.valid_new_ship_placement = True
                 break
+            else:
+                self.ship_row = 0
+                self.ship_col = 0
+                attempts += 1
 
     def find_new_ship_placement_points(self):
         self.ship_points = []
@@ -68,16 +76,17 @@ class BotInterface:
                 counter -= 1
     
     def random_ship_placement_helper(self):
-        self.ship_orientation = random.sample(['v', 'h'], 1)
+        self.ship_orientation = random.choice(['v', 'h'])
         self.random_col_row_placement()
         self.find_new_ship_placement_points()
         self.checking_all_point = [self.game.ship_at(pair_row_col[0], pair_row_col[1]) for pair_row_col in self.ship_points]
+        # print('self.checking_all_point: ', self.checking_all_point)
 
     def random_ship_placement(self):
         self.ship_length = random.choice(self.ships_unplaced())
         self.random_ship_placement_helper()
-        print('self.ship_length: ', self.ship_length)
-        print('self.ship_orientation: ', self.ship_orientation)
+        # print('self.ship_length: ', self.ship_length)
+        # print('self.ship_orientation: ', self.ship_orientation)
 
         while True in self.checking_all_point:
             self.valid_new_ship_placement = False
@@ -101,6 +110,3 @@ class BotInterface:
                     row_cells.append(".")
             rows.append("".join(row_cells))
         return "\n".join(rows)
-
-    # def show_board_to_dev(self):
-    #     return self._format_board()
